b5a7d2edec979625c233bed1bd82310eeefd7fc9
A. <anushkabhave8@gmail.com>
2024-03-28 22:38:03 -0500

    added menu and welcome message

---
+++ main.py
@@ -0,0 +1,12 @@
+def menuScreen():
+	print("-----------------------------------------------------------")
+	print("--------------------DATABASING PROJECT --------------------")
+	print("-----------------------------------------------------------")
+	print()
+
+	name = input("What is your name? >>> ")
+	print()
+	name = name.capitalize()
+	print(f"Welcome {name}! We're happy to see you here.")
+
+menuScreen()
