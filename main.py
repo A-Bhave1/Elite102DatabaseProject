343b4ede8b692a6fe868b0f48a8479ab57d1cdc3
A. <anushkabhave8@gmail.com>
2024-04-02 18:17:04 -0500

    trying one more time? did I mess up 😭

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+import mysql.connector
+connection = mysql.connector.connect(user = 'root', database = 'example', password = 'Elite102Code2College@987')
+cursor = connection.cursor()
+
+addData = ("INSERT INTO example.inventory(ID, NameOfItem, Price, CategoryOfItem, DateCreated, NumberSold) VALUES (3, 'Gift Bags', 1.00, 'Party Goods', '2024-04-01', 20);")
+
+cursor.execute(addData)
+
+testQuery = ("SELECT * FROM example.inventory")
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+cursor.close()
+connection.close()
+
+
+
 def menuScreen():
 	print("-----------------------------------------------------------")
 	print("--------------------DATABASING PROJECT --------------------")
@@ -9,4 +28,4 @@ def menuScreen():
 	name = name.capitalize()
 	print(f"Welcome {name}! We're happy to see you here.")
 
-menuScreen()
+#menuScreen()
