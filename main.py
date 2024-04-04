423fe1437a9f33133971b105549c0e9dee567c5f
A. <anushkabhave8@gmail.com>
2024-04-04 18:29:23 -0500

    retry for lesson 4

--- main.py
+++ main.py
@@ -2,22 +2,21 @@ import mysql.connector
 connection = mysql.connector.connect(user = 'root', database = 'example', password = 'Elite102Code2College@987')
 cursor = connection.cursor()
 
-addData = ("INSERT INTO example.inventory(ID, NameOfItem, Price, CategoryOfItem, DateCreated, NumberSold) VALUES (3, 'Gift Bags', 1.00, 'Party Goods', '2024-04-01', 20);")
+#addData = ("INSERT INTO example.inventory(ID, NameOfItem, Price, CategoryOfItem, DateCreated, NumberSold) VALUES (3, 'Gift Bags', 1.00, 'Party Goods', '2024-04-01', 20);")
 
-cursor.execute(addData)
+#cursor.execute(addData)
 
-testQuery = ("SELECT * FROM example.inventory")
-cursor.execute(testQuery)
+#testQuery = ("SELECT * FROM example.inventory")
+#cursor.execute(testQuery)
 
-for item in cursor:
-    print(item)
+#for item in cursor:
+ #   print(item)
 
-cursor.close()
-connection.close()
+#cursor.close()
+#connection.close()
 
 
-
-def menuScreen():
+def menuScreen():	
 	print("-----------------------------------------------------------")
 	print("--------------------DATABASING PROJECT --------------------")
 	print("-----------------------------------------------------------")
@@ -28,4 +27,64 @@ def menuScreen():
 	name = name.capitalize()
 	print(f"Welcome {name}! We're happy to see you here.")
 
-#menuScreen()
+def checkAccountBalance(email):
+	accountBalanceQuery = (f"SELECT * FROM accounts WHERE Email = {email};")
+	cursor.execute(accountBalanceQuery)
+
+	print(f"YOUR ACCOUNT DETAILS:")
+	for item in cursor:
+		print(item)
+
+def depositFunds(email, amountToDeposit):
+	getBalanceQuery = (f"SELECT Balance FROM accounts WHERE Email = {email};") #get the deposit
+	cursor.execute(getBalanceQuery)
+	newBalance = cursor + amountToDeposit
+
+	addNewBalanceQuery = (f"INSERT INTO accounts(Email, Balance) VALUES({email}, {newBalance});")
+	cursor.execute(addNewBalanceQuery)
+	
+
+def withdrawFunds(email, amountToWithdraw):
+	getBalanceQuery = (f"SELECT Balance FROM accounts WHERE Email = {email};") #get the deposit
+	cursor.execute(getBalanceQuery)
+	newBalance = cursor - amountToWithdraw
+	if(newBalance < 0):
+		print("NOT SUFFICIENT FUNDS.")
+		return
+
+	addNewBalanceQuery = (f"INSERT INTO accounts(Email, Balance) VALUES({email}, {newBalance});")
+	cursor.execute(addNewBalanceQuery)
+
+def createNewAccount(email, password, name, balance):
+	print("THANK YOU. PROCESSING...")
+
+	createAccount = (f"INSERT INTO accounts(Name, Email, Password, Balance) VALUES({name},{email},{password},{balance});")
+	cursor.execute(createAccount)
+
+	print("YOUR ACCOUNT INFORMATION:")
+	viewNewAccountInfo = (f"SELECT * FROM accounts WHERE Email = {email};")
+	cursor.execute(viewNewAccountInfo)
+	for item in cursor:
+		print(item)
+	print("IF ANYTHING IS INCORRECT, PLEASE DON'T HESITATE TO CHANGE YOUR ACCOUNT DETAILS.")
+
+def deleteAccount(email):
+	pass
+
+def modifyAccountDetails(email, value, columnToChange):
+	if(columnToChange.lower() == "email"):
+		changeEmail = (f"INSERT INTO accounts(Email) VALUES({value});")
+		cursor.execute(changeEmail)
+
+	elif(columnToChange.lower() == "name"):
+		changeName = (f"INSERT INTO accounts(Email, Name) VALUES({email}, {value});")
+		cursor.execute(changeName)
+		
+	elif(columnToChange.lower() == "password"):
+		changePassword = (f"INSERT INTO accounts(Email, Password) VALUES({email}, {value});")
+		cursor.execute(changePassword)
+
+	else:
+		print("INVALID INPUT.")
+
+
